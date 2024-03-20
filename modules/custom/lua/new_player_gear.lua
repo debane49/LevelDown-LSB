1a95d18b5c9a61777144a5faf54c0cad66c489a9
General <75862857+FFXILevelDown@users.noreply.github.com>
2024-03-19 21:31:16 -0600

    Update new player module to include naming pets and adding dice and attachments
    added command to name pets and adding dice and attachments to existing players

--- modules/custom/lua/new_player_gear.lua
+++ modules/custom/lua/new_player_gear.lua
@@ -25,6 +25,9 @@ m:addOverride('xi.player.charCreate', function(player)
             player:addFame(xi.quest.fame_area.JEUNO, 30000)
             player:addFame(xi.quest.fame_area.NORG, 30000)
             player:addFame(xi.quest.fame_area.WINDURST, 30000)
+            -- pet names
+            player:setPetName(xi.petType.WYVERN, math.random(1,32))
+            player:setPetName(xi.petType.AUTOMATON, math.random(118, 149))
         end
 
         if not player:hasItem(xi.item.SAN_DORIAN_RING) or
@@ -125,6 +128,51 @@ m:addOverride('xi.player.charCreate', function(player)
 
     }
 
+    -- Corsair Rolls
+local ValidRolls = {
+    98, -- Fighter's Roll
+    99, -- Monk's Roll
+    100, -- Healer's Roll
+    101, -- Wizard's Roll
+    102, -- Warlock's Roll
+    103, -- Rogue's Roll
+    104, -- Gallant's Roll
+    105, -- Chaos Roll
+    106, -- Beast Roll
+    107, -- Choral Roll
+    108, -- Hunter's Roll
+    109, -- Samurai Roll
+    110, -- Ninja Roll
+    111, -- Drachen Roll
+    112, -- Evoker's Roll
+    113, -- Magus' Roll
+    114, -- Corsair's Roll
+    115, -- Puppet's Roll
+    116, -- Dancer's Roll
+    117, -- Scholar's Roll
+    118, -- Bolter's Roll
+    119, -- Caster's Roll
+    120, -- Courser's Roll
+    121, -- Blitzer's Roll
+    122, -- Tactician's Roll
+    302, -- Allies Roll
+    303, -- Miser's Roll
+    304, -- Companion's Roll
+    305, -- Avenger's Roll
+    390, -- Naturalist's Roll
+    391, -- Runeist's Roll
+}
+local ValidAttachments = {
+    8193, 8194, 8195, 8196, 8197, 8198, 8224, 8225, 8226, 8227,
+    8449, 8450, 8451, 8452, 8453, 8454, 8455, 8456, 8458,
+    8462, 8481, 8482, 8483, 8484, 8485, 8486, 8487, 8488, 8489, 8490, 8491, 8493, 8497,
+    8513, 8514, 8515, 8516, 8517, 8518, 8519, 8520,
+    8523, 8524, 8526, 8528, 8545, 8546, 8547, 8548, 8549, 8550, 8551,
+    8555, 8577, 8578, 8579, 8580, 8581, 8582, 8583, 8584, 8585,
+    8587, 8609, 8610, 8611, 8612, 8613, 8614, 8615, 8616,
+    8619, 8641, 8642, 8643, 8644, 8645, 8646, 8648, 8651,
+    8673, 8674, 8675, 8676, 8677, 8678, 8680, 8682, 9138, 9143, 9230,
+}
     -- add all spells
     local save = true
     local silent = true
@@ -137,6 +185,24 @@ m:addOverride('xi.player.charCreate', function(player)
         end
 
         targ:addSpell(validSpells[i], silent, save, sendUpdate)
+    end
+    -- add in rolls
+    for i = 1, #ValidRolls do
+        if i == #ValidRolls then
+            silent = false
+            sendUpdate = true
+        end
+
+        targ:addLearnedAbility(ValidRolls[i], silent, save, sendUpdate)
+    end
+    -- add in attachments
+    for i = 1, #ValidAttachments do
+        if i == #ValidAttachments then
+            silent = false
+            sendUpdate = true
+        end
+
+        targ:unlockAttachment(ValidAttachments[i], silent, save, sendUpdate)
     end
 
 ----------------------------------------
