b89a9495dd904de3b65170456d8dbe433da4c900
Barthandalus <Drobinsona149@hotmail.com>
2024-03-21 23:41:09 -0400

    update help command to include oldccfix and retrievealex

--- modules/custom/commands/help.lua
+++ modules/custom/commands/help.lua
@@ -1,6 +1,6 @@
 -----------------------------------
 -- func: help prints out list of player commands
--- desc: 
+-- desc:
 -----------------------------------
 local commandObj = {}
 
@@ -35,6 +35,8 @@ player:printToPlayer('!library - Teleports you to Celennia Memorial Library', 3,
 player:printToPlayer('!ah - Allows you to open up the AH anywhere in the world', 3, xi.msg.channel.system_3)
 player:printToPlayer('!posfix - Fixes a characters pos who is offline but shows to be online', 3, xi.msg.channel.system_3)
 player:printToPlayer('!resetme - Forces your character to rezone and return to same pos', 3, xi.msg.channel.system_3)
+player:printToPlayer('!retrievealex - Gives the specified amount of alexandrite', 3, xi.msg.channel.system_3)
+player:printToPlayer('!oldccfix - Adds all valid spells EXCEPT TRUSTS to the given target. If no target then to the current player.', 3, xi.msg.channel.system_3)
 
 
 
