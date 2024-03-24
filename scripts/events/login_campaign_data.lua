7175c69758568e4bcf0c532df15a9d6ae412e168
General <75862857+FFXILevelDown@users.noreply.github.com>
2024-03-23 20:25:29 -0600

    Update Login Campain dates and rewards

--- scripts/events/login_campaign_data.lua
+++ scripts/events/login_campaign_data.lua
@@ -13,24 +13,33 @@ local prizes =
         ['price'] = 10,
         ['items'] =
         {
-            1126, -- Beastmen's Seal
-            1127, -- Kindred's Seal
-            2955, -- Kindred's Crest
-            2956, -- High Kindred's Crest
-            2957, -- Sacred Kindred's Crest
-            -- 1857, -- Cordial Invite
-            -- 2306, -- Martial Ball Invite
-            -- 5364, -- Training Grounds Key
-            -- 2487, -- Mercenary Camp Entry Slip
-            -- 5741, -- Flask Of Pest Repellent
-            -- 3557, -- Athena Orb
-            -- 5113, -- Cracked Nut
-            -- 3541, -- Seasoning Stone
-            -- 3543, -- Fossilized Fang
-            -- 3542, -- Fossilized Bone
-            -- 5724, -- Pungent Powder
-            -- 6535, -- Pungent Powder II
-            -- 9890, -- Tarazacum Orb
+--- Alway active ----------------------------------------
+1126, -- beastmens_seal
+1127, -- kindreds_seal
+2955, -- kindreds_crest
+2956, -- h._kindred_crest
+2957, -- s._kindred_crest
+1455, -- one byne bill
+1452, -- ordelle bronzepiece
+1449, -- tukuku whiteshell
+1875, -- ancient beastcoin
+--- Month 1 ---------------------------------------------
+6377, -- imperial chair set
+6378, -- decorative chair set
+6379, -- ornate stool set
+--- Month 2 ---------------------------------------------
+--[[
+6380, -- refined chair set
+6408, -- portable container
+6409, -- ephramadian throne
+]]--
+--- Month 3 ---------------------------------------------
+--[[
+6410, -- shadow throne
+6411, -- chocobo chair
+6412, -- leaf bench
+6413, -- astral cube
+]]--
         },
     },
 
@@ -39,26 +48,41 @@ local prizes =
         ['price'] = 100,
         ['items'] =
         {
-            8734,  -- Mog Kupon I-S1
-            8966,  -- Eudaemon Blade
-            8967,  -- Eudaemon Cape
-            8968,  -- Eudaemon Ring
-            8969,  -- Eudaemon Sash
-            8970,  -- Eudaemon Shield
-            17006, -- Drill Calamary
-            17007, -- Dwarf Pugil
-            6413,  -- Astral Cube
-            -- 9891,  -- Zinnia Orb
-            -- 10112, -- Cipher: Zeid
-            -- 10113, -- Cipher: Lion
-            10118, -- Cipher: Naja
-            10120, -- Cipher: Lehko
-            -- 10124, -- Cipher: Luzaf
-            -- 10125, -- Cipher: Najelith
-            -- 10129, -- Cipher: Domina
-            -- 10134, -- Cipher: S. Sibyl
-            -- 10142, -- Cipher: Karaha
-            -- 10149, -- Cipher: Areuhat
+--- Month 1 ---------------------------------------------
+10124, -- cipher of luzafs alter ego
+10125, -- cipher of najeliths alter ego
+10112, -- cipher of zeids alter ego
+10113, -- cipher of lions alter ego
+10065, -- ♪spectral chair
+10066, -- ♪spheroid
+10067, -- ♪omega
+10068, -- ♪coeurl
+10069, -- ♪goobbue
+10070, -- ♪raaz
+10129, -- cipher of d. shantottos alter ego
+--- Month 2 ---------------------------------------------
+--[[
+10134, -- cipher of star sibyls alter ego
+10142, -- cipher of karahas alter ego
+10149, -- cipher of areuhats alter ego
+10071, -- ♪levitus
+10072, -- ♪adamantoise
+10073, -- ♪dhalmel
+10074, -- ♪doll
+10075, -- ♪red raptor
+10076, -- ♪golden bomb
+]]--
+--- Month 3 ---------------------------------------------
+--[[
+10179, -- cipher of teodors alter ego
+10183, -- cipher of darrcuilns alter ego
+10178, -- cipher of ullegores alter ego
+10077, -- ♪buffalo
+10078, -- ♪wivre
+10079, -- ♪iron giant
+10080, -- ♪byakko
+10082, -- ♪ixion
+]]--
         },
     },
 
@@ -67,21 +91,41 @@ local prizes =
         ['price'] = 100,
         ['items'] =
         {
-            -- 10136, -- Cipher: Uka
-            -- 10141, -- Cipher: Kuyin
-            -- 10144, -- Cipher: Abenzio
-            -- 10145, -- Cipher: Rughadjeen
-            -- 10150, -- Cipher: Lhe
-            -- 10151, -- Cipher: Mayakov
-            -- 10155, -- Cipher: Brygid
-            -- 10156, -- Cipher: Mildaurion
-            -- 10161, -- Cipher: Rongelouts
-            -- 10166, -- Cipher: Robel-Akbel
-            -- 10178, -- Cipher: Ullegore
-            -- 10179, -- Cipher: Teodor
-            -- 10183, -- Cipher: Darrcuiln
-            20713, -- Excalipoor
-            6008,  -- Copse Candy
+--- Month 1 ---------------------------------------------
+10136, -- cipher of ukas alter ego
+10141, -- cipher of kuyins alter ego
+10144, -- cipher of abenzios alter ego
+10145, -- cipher of rughadjeens alter ego
+10049, -- ♪raptor
+10050, -- ♪tiger
+10051, -- ♪crab
+10052, -- ♪red crab
+10053, -- ♪bomb
+10054, -- ♪ram
+10150, -- cipher of lhes alter ego
+--- Month 2 ---------------------------------------------
+--[[
+10151, -- cipher of mayakovs alter ego
+10155, -- cipher of brygids alter ego
+10156, -- cipher of mildaurions alter ego
+10055, -- ♪morbol
+10056, -- ♪crawler
+10057, -- ♪fenrir
+10058, -- ♪beetle
+10059, -- ♪moogle
+10060, -- ♪magic pot
+]]--
+--- Month 3 ---------------------------------------------
+--[[
+10161, -- cipher of rongeloutss alter ego
+10166, -- cipher of robel-akbels alter ego
+10187, -- cipher of shantottos alter ego ii
+10061, -- ♪tulfaire
+10062, -- ♪warmachine
+10063, -- ♪xzomit
+10064, -- ♪hippogryph
+10083, -- ♪phuabo
+]]--
         },
     },
 
@@ -90,27 +134,40 @@ local prizes =
         ['price'] = 300,
         ['items'] =
         {
-            -- 10187, -- Cipher: Shantotto II
-            10069, -- ♪Goobbue
-            10051, -- ♪Crab
-            10058, -- ♪Beetle
-            -- TODO: The other mounts
-            10384, -- Cumulus Masque
-            20666, -- Blizzard Brand
-            -- 25658, -- Wyrm. Masque +1
-            -- 25757, -- Wyrmking Suit +1
-            -- 5854,  -- Frayed Pouch (B)
-            -- 5855,  -- Frayed Pouch (A)
-            -- 5856,  -- Frayed Pouch (G)
-            -- 5857,  -- Frayed Pouch (D)
-            -- 5858,  -- Frayed Pouch (R)
-            -- 5946,  -- Frayed Sack (D)
-            -- 5947,  -- Frayed Sack (L)
-            -- 4064,  -- Rem's Tale Ch.1
-            -- 4065,  -- Rem's Tale Ch.2
-            -- 4066,  -- Rem's Tale Ch.3
-            -- 4067,  -- Rem's Tale Ch.4
-            -- 4068,  -- Rem's Tale Ch.5
+--- Alway active ----------------------------------------
+4064, -- copy of rems tale, chapter 1
+4065, -- copy of rems tale, chapter 2
+4066, -- copy of rems tale, chapter 3
+4067, -- copy of rems tale, chapter 4
+4068, -- copy of rems tale, chapter 5
+--- Month 1 ---------------------------------------------
+27760, -- chocobo masque +1
+27906, -- chocobo suit +1
+25607, -- Starlet Flower
+26975, -- Starlet Jabot
+27112, -- Starlet Gloves
+27297, -- Starlet Skirt
+27468, -- Starlet Boots
+--- Month 2 ---------------------------------------------
+--[[
+25658, -- Wyrm. Masque +1
+25757, -- Wyrmking Suit +1
+25606, -- Agent Hood
+26974, -- Agent Coat
+27111, -- Agent Cuffs
+27296, -- Agent Pants
+27467, -- Agent Boots
+]]--
+--- Month 3 ---------------------------------------------
+--[[
+1854, -- deed of moderation
+1851, -- deed of placidity
+1870, -- deed of sensibility
+26799, -- behemoth masque +1
+26955, -- behemoth suit +1
+10250, -- moogle suit
+10429, -- moogle masque
+]]--
         },
     },
 
@@ -119,8 +176,40 @@ local prizes =
         ['price'] = 500,
         ['items'] =
         {
-            -- TODO: Special month-to-month things
-            10073, -- ♪Dhalmel
+--- Alway active ----------------------------------------
+15515, -- peacock amulet
+15351, -- bounding boots
+14986, -- ochimusha kote
+13250, -- sonic belt +1
+15737, -- sarutobi kyahan
+--- Month 1 ---------------------------------------------
+20713, -- Excalipoor
+20666, -- Blizzard Brand
+11957, -- novennial thigh boots
+11854, -- Novennial Dress
+10431, -- decennial tiara
+10252, -- decennial dress
+10593, -- Decennial tights
+--- Month 2 ---------------------------------------------
+--[[
+20668, -- Fire Tongue
+21965, -- Zanmato
+10594, -- decennial hose
+10430, -- Decennial Crown
+10251, -- Decennial Coat
+11853, -- Novennial Coat
+11953, -- Novennial Hose
+]]--
+--- Month 3 ---------------------------------------------
+--[[
+1850, -- first virtue
+1853, -- second virtue
+1856, -- third virtue
+21867, -- Sha Wujing Lance
+20665, -- kamlanauts sword
+11355, -- Dinner Jacket
+16378, -- Dinner Hose
+]]--
         },
 
     },
@@ -130,23 +219,37 @@ local prizes =
         ['price'] = 750,
         ['items'] =
         {
-            3885, -- Melodious Plans
-            3886, -- Timbre Case Kit
-            3887, -- Musichinery Kit
-            -- 9079, -- Kitchen Brick
-            -- 9080, -- Kitchen Stove
-            -- 9081, -- Kitchen Plate
-            3339, -- Honey Wine
-            3341, -- Beastly Shank
-            3343, -- Blue Pondweed
-            -- 1873, -- Brigand's Chart
-            -- 1874, -- Pirate's Chart
-            -- 6381, -- Fisherman's Feast
-            -- 4069, -- Copy Of Rem's Tale, Chapter 6
-            -- 4070, -- Copy Of Rem's Tale, Chapter 7
-            -- 4071, -- Copy Of Rem's Tale, Chapter 8
-            -- 4072, -- Copy Of Rem's Tale, Chapter 9
-            -- 4073, -- Copy Of Rem's Tale, Chapter 10
+--- Alway active ----------------------------------------
+4069, -- copy of rems tale, chapter 6
+4070, -- copy of rems tale, chapter 7
+4071, -- copy of rems tale, chapter 8
+4072, -- copy of rems tale, chapter 9
+4073, -- copy of rems tale, chapter 10
+3355, -- Divine Bijou
+3353, -- Barbaric Bijou
+3354, -- Steelwall BiJou
+3356, -- Roving Bijou
+--- Month 1 ---------------------------------------------
+3339, -- Honey Wine
+3341, -- Beastly Shank
+3343, -- Blue Pond Water
+--- Month 2 ---------------------------------------------
+--[[
+1419, -- Spring Stone
+1421, -- Summer Stone
+1423, -- Autumn Stone
+1425, -- Winter Stone
+1418, -- Gem of the East
+1422, -- Gem of the West
+1424, -- Gem of the North
+1420, -- Gem of the South
+]]--
+--- Month 3 ---------------------------------------------
+--[[
+1848, -- fourth virtue
+1847, -- fifth virtue
+1849, -- sixth virtue
+]]--
         },
     },
 
@@ -155,12 +258,38 @@ local prizes =
         ['price'] = 1000,
         ['items'] =
         {
-            -- 6499,  -- Patio Design Plans
-            26165, -- Facility Ring
-            26164, -- Caliber Ring
-            -- 6486,  -- Frayed Sack (Pel)
-            -- 6487,  -- Frayed Sack (Fer)
-            -- 6488,  -- Frayed Sack (Tau)
+--- Alway active ----------------------------------------
+14739, -- Suppanomimi
+14741, -- Abyssal Earring
+14742, -- Beastly Earring
+14743, -- Bushinomimi Earring
+14740, -- Knights Earring
+1450, -- lungo-nango jadeshell
+1453, -- montiont silverpiece
+1456, -- one hundred byne bill
+3357, -- leering bijou
+3456, -- creepers juju
+3457, -- revelatory juju
+3458, -- undying juju
+3459, -- heralds juju
+3358, -- Shrouded Bijou
+--- Month 1 ---------------------------------------------
+15543, -- rajas ring
+15544, -- sattva ring
+15545, -- tamas ring
+--- Month 2 ---------------------------------------------
+--[[
+15807, -- balrahns ring
+15808, -- ulthalams ring
+15809, -- jalzahns ring
+]]--
+--- Month 3 ---------------------------------------------
+--[[
+15962, -- static earring
+15963, -- magnetic earring
+15964, -- hollow earring
+15965, -- ethereal earring
+]]--
         },
     },
 
@@ -169,14 +298,32 @@ local prizes =
         ['price'] = 1500,
         ['items'] =
         {
-            3340, -- Sweet Tea
-            3342, -- Savory Shank
-            3344, -- Red Pondweed
-            -- 8720, -- Maliya. Coral Orb
-            -- 8722, -- Hepatizon Ingot
-            -- 8724, -- Beryllium Ingot
-            -- 8726, -- Exalted Lumber
-            -- 8728, -- Sif's Macrame
+--- Alway active ----------------------------------------
+3340, -- Sweet Tea
+3342, -- Savory Shank
+3344, -- Red Pondweed
+8720, -- Maliya. Coral Orb
+--- Month 1 ---------------------------------------------
+10193, -- cipher of monberauxs alter ego
+28419, -- hachirin-no-obi
+18852, -- octave club
+25679, -- white rarab cap +1
+--- Month 2 ---------------------------------------------
+--[[
+8722, -- Hepatizon Ingot
+8724, -- Beryllium Ingot
+10120, -- cipher of lehkos alter ego
+19163, -- nightfall
+28450, -- chaac belt
+]]--
+--- Month 3 ---------------------------------------------
+--[[
+8726, -- Exalted Lumber
+8728, -- Sif's Macrame
+27510, -- fotia gorget
+28420, -- fotia belt
+27585, -- gorney ring
+]]--
         },
     },
 }
