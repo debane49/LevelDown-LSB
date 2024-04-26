3687dcb58edb91d1c4a2cda9af8e858753dca74c
General <75862857+FFXILevelDown@users.noreply.github.com>
2024-04-25 22:32:43 -0600

    Updated xp guide to include more player found xp spots.

--- modules/custom/lua/xp_guide.lua
+++ modules/custom/lua/xp_guide.lua
@@ -1,11 +1,7 @@
 -----------------------------------
--- XP Book Northern Sandoria
+-- XP / Merit Books
 -----------------------------------
 require('modules/module_utils')
---require('scripts/zones/Northern_San_dOria/Zone')
---require('scripts/zones/Bastok_Mines/Zone')
---require('scripts/zones/Port_Windurst/Zone')
---require('scripts/zones/RuLude_Gardens/Zone')
 -----------------------------------
 local m = Module:new('xp_guide')
 
@@ -19,6 +15,9 @@ local page5 = {}
 local page6 = {}
 local page7 = {}
 local page8 = {}
+local page9 = {}
+local page10 = {}
+local page11 = {}
 
 local delaySendMenu = function(player)
     player:timer(50, function(playerArg)
@@ -109,17 +108,17 @@ page3 =
         function(playerArg)
             playerArg:setPos(-187.5810, 8.0470, -73.6966, 210, xi.zone.CAPE_TERIGGAN)
         end,
-    }, 
+    },
+    {
+        'Lvl 55-65 Wajaom Woodlands', --  - Page 1 RoE
+        function(playerArg)
+            playerArg:setPos(-200.1854, -10.0000, 91.8009, 53, xi.zone.WAJAOM_WOODLANDS)
+        end,
+    },
    {
         'Lvl 55-69 The Boyahda Tree', -- - Page 1 RoE
         function(playerArg)
             playerArg:setPos(339.7918, 8.6358, -69.4237, 181, xi.zone.THE_BOYAHDA_TREE)
-        end,
-    },
-   {
-        'Lvl 68-80 Misareaux Coast', -- - Page 1 RoE
-        function(playerArg)
-            playerArg:setPos(-44.6588, -32.3265, 274.5248, 230, xi.zone.MISAREAUX_COAST)
         end,
     },
     {
@@ -140,17 +139,23 @@ page3 =
 page4 = 
 {
    {
-        'Lvl 80-90 Dangruf Wadi', -- - Page 8 RoE
+        'Lvl 68-76 RuAun Gardens', -- - Page 1 RoE
         function(playerArg)
-            playerArg:setPos(-16.6683, -0.2101, -0.0809, 146, xi.zone.DANGRUF_WADI)
+            playerArg:setPos(-0.6167, -44.0130, -165.7867, 185, xi.zone.RUAUN_GARDENS)
         end,
     },
-    {
-        'Lvl 90-99 Gustav Tunnel', --  - Page 8 RoE
+   {
+        'Lvl 68-80 Misareaux Coast', -- - Page 1 RoE
         function(playerArg)
-            playerArg:setPos(-60.2877, -10.6451, -135.5146, 195, xi.zone.GUSTAV_TUNNEL)
+            playerArg:setPos(-44.6588, -32.3265, 274.5248, 230, xi.zone.MISAREAUX_COAST)
         end,
-    }, 
+    },
+   {
+        'Lvl 70-82 The Shrine of RuAvitau', -- - 
+        function(playerArg)
+            playerArg:setPos(4.7311, -15.5148, -2.4892, 68, xi.zone.THE_SHRINE_OF_RUAVITAU)
+        end,
+    },
     {
         'Previous Page',
         function(playerArg)
@@ -158,7 +163,103 @@ page4 =
             delaySendMenu(playerArg)
         end,
     },
+    {
+        'Next Page',
+        function(playerArg)
+            menu.options = page9
+            delaySendMenu(playerArg)
+        end,
+    },
 }
+page9 =
+{
+   {
+        'Lvl 73-82 The Shrine of RuAvitau', -- - Pa
+        function(playerArg)
+            playerArg:setPos(740.4669, -99.6000, -655.9205, 230, xi.zone.THE_SHRINE_OF_RUAVITAU)
+        end,
+    },
+  {
+        'Lvl 80-90 Dangruf Wadi', -- - Page 8 RoE
+        function(playerArg)
+            playerArg:setPos(-16.6683, -0.2101, -0.0809, 146, xi.zone.DANGRUF_WADI)
+        end,
+    },
+   {
+        'Lvl 80-90 Mount Zhayolm', -- - Page 8 RoE
+        function(playerArg)
+            playerArg:setPos(-580.4277, -23.6067, -34.7014, 64, xi.zone.MOUNT_ZHAYOLM)
+        end,
+    },
+    {
+        'Previous Page',
+        function(playerArg)
+            menu.options = page4
+            delaySendMenu(playerArg)
+        end,
+    },
+    {
+        'Next Page',
+        function(playerArg)
+            menu.options = page10
+            delaySendMenu(playerArg)
+        end,
+    },
+
+}
+page10 =
+{
+   {
+        'Lvl 82-96 Crawlers Nest', -- - 
+        function(playerArg)
+            playerArg:setPos(12.6418, -0.3837, -299.6174, 16, xi.zone.CRAWLERS_NEST)
+        end,
+    },
+   {
+        'Lvl 85-95 Garlaige Citadel', -- - Page 8 RoE
+        function(playerArg)
+            playerArg:setPos(-378.7412, -5.9991, 361.1081, 62, xi.zone.GARLAIGE_CITADEL)
+        end,
+    },
+    {
+        'Lvl 90-99 Gustav Tunnel', --  - Page 8 RoE
+        function(playerArg)
+            playerArg:setPos(-60.2877, -10.6451, -135.5146, 195, xi.zone.GUSTAV_TUNNEL)
+        end,
+    },
+    {
+        'Previous Page',
+        function(playerArg)
+            menu.options = page9
+            delaySendMenu(playerArg)
+        end,
+    },
+    {
+        'Next Page',
+        function(playerArg)
+            menu.options = page11
+            delaySendMenu(playerArg)
+        end,
+    },
+
+}
+page11 =
+{
+    {
+        'Lvl 90-99 Ceizak Battlegrounds', 
+        function(playerArg)
+            playerArg:setPos(329.6800, 0.3515, 126.9239, 72, xi.zone.CEIZAK_BATTLEGROUNDS)
+        end,
+    },
+    {
+        'Previous Page',
+        function(playerArg)
+            menu.options = page10
+            delaySendMenu(playerArg)
+        end,
+    },
+}
+
 page5 =
 {
     {
@@ -284,26 +385,19 @@ page5 =
         end,
     },
 }
---[[
-Need access- 6k CP/kill 99+
---Escha-Zitah (F-6) - Dragons and puks - 288 ESCHA_ZITAH
---Escha-Zitah (L-10) Yztarg and Opo-opo  - 288 ESCHA_ZITAH
 
-Has access - 10k+ CP/kill SU2+
-----Ra'Kaznar Inner Court (L-4) - 276  RAKAZNAR_INNER_COURT 
 
-Has access - 15k+ CP/kill SU3+
--- Dho gates (J-11) Apex mobs - 272 DHO_GATES
---Moh gates (K-6) Apex mobs - 269 MOH_GATES
-Sih gates (K-7) Apex mobs - 268 SIH_GATES
-Sih gates (L-8) Apex mobs - 268 SIH_GATES
-Sih gates (J-9) Apex mobs - 268 SIH_GATES
---Reisenjima - any mob - 291 REISENJIMA
 
-Has access - 60k+ CP/kill SU3+ PT
-----Bhaflau Thickets (F-5) Colibri - 52 BHAFLAU_THICKETS
-----King Ranperre's Tomb (H-9) Beetles  - 190 KING_RANPERRES_TOMB
-]]--
+
+
+
+
+
+
+
+
+
+
 m:addOverride('xi.zones.Bastok_Mines.Zone.onInitialize', function(zone)
     super(zone)
     local exp_guidetwo = zone:insertDynamicEntity({
