896e1672e1e98fa8d4014c9f7a63bb9260238548
General <75862857+FFXILevelDown@users.noreply.github.com>
2024-05-18 19:44:00 -0600

    Update Sea and Sky mobs difficulty and additonal drops

---
+++ modules/custom/sql/skyseaUpdates.sql
@@ -0,0 +1,190 @@
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  75000, MP =    0 WHERE name = 'Mother_Globe'         AND groupid =   9;
+UPDATE `mob_groups` SET minLevel =  85, maxLevel =  85, HP =      0, MP =    0 WHERE name = 'Slave_Globe'          AND groupid =  10;
+UPDATE `mob_groups` SET minLevel =  85, maxLevel =  85, HP =      0, MP =    0 WHERE name = 'Slave_Globe'          AND groupid =  10;
+UPDATE `mob_groups` SET minLevel =  85, maxLevel =  85, HP =      0, MP =    0 WHERE name = 'Slave_Globe'          AND groupid =  10;
+UPDATE `mob_groups` SET minLevel =  85, maxLevel =  85, HP =      0, MP =    0 WHERE name = 'Slave_Globe'          AND groupid =  10;
+UPDATE `mob_groups` SET minLevel =  85, maxLevel =  85, HP =      0, MP =    0 WHERE name = 'Slave_Globe'          AND groupid =  10;
+UPDATE `mob_groups` SET minLevel =  85, maxLevel =  85, HP =      0, MP =    0 WHERE name = 'Slave_Globe'          AND groupid =  10;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  75000, MP =    0 WHERE name = 'Steam_Cleaner'        AND groupid =  15;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  75000, MP =    0 WHERE name = 'Faust'                AND groupid =   6;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  75000, MP = 8500 WHERE name = 'Brigandish_Blade'     AND groupid =  14;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  75000, MP =    0 WHERE name = 'Ullikummi'            AND groupid =  12;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  75000, MP =    0 WHERE name = 'Despot'               AND groupid =  13;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  35000, MP = 8500 WHERE name = 'Olla_Pequena'         AND groupid =  24;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  55000, MP = 8500 WHERE name = 'Olla_Media'           AND groupid =  25;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  75000, MP = 8500 WHERE name = 'Olla_Grande'          AND groupid =  26;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  75000, MP =    0 WHERE name = 'Zipacna'              AND groupid =  16;
+UPDATE `mob_groups` SET minLevel = 100, maxLevel = 100, HP = 100000, MP = 8500 WHERE name = 'Seiryu'               AND groupid =  16;
+UPDATE `mob_groups` SET minLevel = 100, maxLevel = 100, HP = 100000, MP = 8500 WHERE name = 'Suzaku'               AND groupid =  18;
+UPDATE `mob_groups` SET minLevel = 100, maxLevel = 100, HP = 100000, MP = 8500 WHERE name = 'Byakko'               AND groupid =  17;
+UPDATE `mob_groups` SET minLevel = 100, maxLevel = 100, HP = 100000, MP = 8500 WHERE name = 'Genbu'                AND groupid =  15;
+UPDATE `mob_groups` SET minLevel = 105, maxLevel = 105, HP = 105000, MP = 8500 WHERE name = 'Kirin'                AND groupid =  27;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =      0, MP =    0 WHERE name = 'Kirins_Avatar'        AND groupid =  32;
+UPDATE `mob_groups` SET minLevel =  95, maxLevel =  95, HP =      0, MP =    0 WHERE name = 'Genbu_pet'            AND groupid = 256;
+UPDATE `mob_groups` SET minLevel =  95, maxLevel =  95, HP =      0, MP =    0 WHERE name = 'Seiryu_pet'           AND groupid = 257;
+UPDATE `mob_groups` SET minLevel =  95, maxLevel =  95, HP =      0, MP =    0 WHERE name = 'Suzaku_pet'           AND groupid = 258;
+UPDATE `mob_groups` SET minLevel =  95, maxLevel =  95, HP =      0, MP =    0 WHERE name = 'Byakko_pet'           AND groupid = 259;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  90000, MP =    0 WHERE name = 'Ixaern_DRG'           AND groupid =  31;
+UPDATE `mob_groups` SET minLevel =  85, maxLevel =  85, HP =      0, MP =    0 WHERE name = 'Ixaern_DRGs_Wynav'    AND groupid =  32;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  90000, MP =    0 WHERE name = 'Ixaern_MNK'           AND groupid =  24;
+UPDATE `mob_groups` SET minLevel =  90, maxLevel =  90, HP =  90000, MP =    0 WHERE name = 'Ixaern_DRK'           AND groupid =  28;
+UPDATE `mob_groups` SET minLevel =  95, maxLevel =  95, HP = 100000, MP = 8500 WHERE name = 'Jailer_of_Fortitude'  AND groupid =  25;
+UPDATE `mob_groups` SET minLevel =  95, maxLevel =  95, HP = 100000, MP = 8500 WHERE name = 'Jailer_of_Temperance' AND groupid =  23;
+UPDATE `mob_groups` SET minLevel =  95, maxLevel =  95, HP = 100000, MP = 8500 WHERE name = 'Jailer_of_Faith'      AND groupid =  30;
+UPDATE `mob_groups` SET minLevel =  95, maxLevel =  95, HP = 100000, MP = 8500 WHERE name = 'Jailer_of_Justice'    AND groupid =  50;
+UPDATE `mob_groups` SET minLevel =  95, maxLevel =  95, HP = 100000, MP = 8500 WHERE name = 'Jailer_of_Hope'       AND groupid =  49;
+UPDATE `mob_groups` SET minLevel =  95, maxLevel =  95, HP = 100000, MP = 8500 WHERE name = 'Jailer_of_Prudence'   AND groupid =  52;
+UPDATE `mob_groups` SET minLevel = 105, maxLevel = 105, HP = 105000, MP = 8500 WHERE name = 'Jailer_of_Love'       AND groupid =  53;
+UPDATE `mob_groups` SET minLevel = 110, maxLevel = 110, HP = 110000, MP = 8500 WHERE name = 'Absolute_Virtue'      AND groupid =  56;
+
+UPDATE mob_groups SET dropid = 3919 WHERE name = 'Ixaern_mnk' AND groupid = 24;
+
+INSERT INTO mob_droplist VALUES(2820,1,1,1000,4064,250);-- Rem tale 1 / NMs Mother Globe
+INSERT INTO mob_droplist VALUES(2820,1,1,1000,4069,250);-- Rem tale 6 / NMs Mother Globe
+INSERT INTO mob_droplist VALUES(2820,1,2,1000,844,250);-- Phoenix Feather / NMs Mother Globe
+INSERT INTO mob_droplist VALUES(2820,1,2,1000,1311,250);-- Oxblood / NMs Mother Globe
+INSERT INTO mob_droplist VALUES(2820,1,3,1000,16605,333);-- Enhancing Sword / NMs Mother Globe
+INSERT INTO mob_droplist VALUES(2820,1,3,1000,20628,333);-- Mindmeld Kris / NMs Mother Globe
+INSERT INTO mob_droplist VALUES(2820,1,3,1000,28571,334);-- Waterfall Ring / NMs Mother Globe
+INSERT INTO mob_droplist VALUES(2326,1,1,1000,4065,250);-- Rem tale 2 / NMs Steam Cleaner
+INSERT INTO mob_droplist VALUES(2326,1,1,1000,4066,250);-- Rem tale 3 / NMs Steam Cleaner
+INSERT INTO mob_droplist VALUES(2326,1,2,1000,837,250);-- Malboro Fiber / NMs Steam Cleaner
+INSERT INTO mob_droplist VALUES(2326,1,2,1000,844,250);-- Phoenix Feather / NMs Steam Cleaner
+INSERT INTO mob_droplist VALUES(2326,1,3,1000,28657,500);-- Kaidate / NMs Steam Cleaner
+INSERT INTO mob_droplist VALUES(2326,1,3,1000,20625,500);-- Secespita / NMs Steam Cleaner
+INSERT INTO mob_droplist VALUES(2821,1,1,1000,4066,250);-- Rem tale 3 / NMs Faust
+INSERT INTO mob_droplist VALUES(2821,1,1,1000,4067,250);-- Rem tale 4 / NMs Faust
+INSERT INTO mob_droplist VALUES(2821,1,2,1000,1110,250);-- Beetle Blood / NMs Faust
+INSERT INTO mob_droplist VALUES(2821,1,2,1000,837,250);-- Malboro Fiber / NMs Faust
+INSERT INTO mob_droplist VALUES(2821,1,3,1000,28512,333);-- Dawn Earring / NMs Faust
+INSERT INTO mob_droplist VALUES(2821,1,3,1000,28574,333);-- Icecrack Ring / NMs Faust
+INSERT INTO mob_droplist VALUES(2821,1,3,1000,21120,3334);-- Patriarch Cane / NMs Faust
+INSERT INTO mob_droplist VALUES(357,1,1,1000,4067,250);-- Rem tale 4 / NMs Brigandish Blade
+INSERT INTO mob_droplist VALUES(357,1,1,1000,4068,250);-- Rem tale 5 / NMs Brigandish Blade
+INSERT INTO mob_droplist VALUES(357,1,2,1000,836,250);-- Damascene Cloth / NMs Brigandish Blade
+INSERT INTO mob_droplist VALUES(357,1,2,1000,1110,250);-- Beetle Blood / NMs Brigandish Blade
+INSERT INTO mob_droplist VALUES(357,1,3,1000,28304,500);-- Litany Clogs / NMs Brigandish Blade
+INSERT INTO mob_droplist VALUES(357,1,3,1000,28383,500);-- Pentalagus Charm / NMs Brigandish Blade
+INSERT INTO mob_droplist VALUES(2822,1,1,1000,4068,250);-- Rem tale 5 / NMs Ullikummi
+INSERT INTO mob_droplist VALUES(2822,1,1,1000,4065,250);-- Rem tale 2 / NMs Ullikummi
+INSERT INTO mob_droplist VALUES(2822,1,2,1000,1311,250);-- Oxblood / NMs Ullikummi
+INSERT INTO mob_droplist VALUES(2822,1,2,1000,836,250);-- Damascene Cloth / NMs Ullikummi
+INSERT INTO mob_droplist VALUES(2822,1,3,1000,21382,500);-- Dosis Tathlum / NMs Ullikummi
+INSERT INTO mob_droplist VALUES(2822,1,3,1000,20956,500);-- Sibat / NMs Ullikummi
+INSERT INTO mob_droplist VALUES(638,1,1,1000,4069,250);-- Rem tale 6 / NMs Despot
+INSERT INTO mob_droplist VALUES(638,1,1,1000,4064,250);-- Rem tale 1 / NMs Despot
+INSERT INTO mob_droplist VALUES(638,1,2,1000,844,250);-- Phoenix Feather / NMs Despot
+INSERT INTO mob_droplist VALUES(638,1,2,1000,1311,250);-- Oxblood / NMs Despot
+INSERT INTO mob_droplist VALUES(638,1,3,1000,28165,333);-- Laktisma Leggings / NMs Despot
+INSERT INTO mob_droplist VALUES(638,1,3,1000,20542,333);-- Gnafrons Adargas / NMs Despot
+INSERT INTO mob_droplist VALUES(638,1,3,1000,28027,334);-- Boor Bracelets / NMs Despot
+INSERT INTO mob_droplist VALUES(2823,1,1,1000,4064,250);-- Rem tale 1 / NMs Olla Grande
+INSERT INTO mob_droplist VALUES(2823,1,1,1000,4065,250);-- Rem tale 2 / NMs Olla Grande
+INSERT INTO mob_droplist VALUES(2823,1,2,1000,836,250);-- Damascene Cloth / NMs Olla Grande
+INSERT INTO mob_droplist VALUES(2823,1,2,1000,844,250);-- Phoenix Feather / NMs Olla Grande
+INSERT INTO mob_droplist VALUES(2823,1,3,1000,20629,500);-- Legato Dagger / NMs Olla Grande
+INSERT INTO mob_droplist VALUES(2823,1,3,1000,28456,500);-- Kasiri Belt / NMs Olla Grande
+INSERT INTO mob_droplist VALUES(2800,1,1,1000,4066,250);-- Rem tale 3 / NMs Zipacna
+INSERT INTO mob_droplist VALUES(2800,1,1,1000,4067,250);-- Rem tale 4 / NMs Zipacna
+INSERT INTO mob_droplist VALUES(2800,1,2,1000,1110,250);-- Beetle Blood / NMs Zipacna
+INSERT INTO mob_droplist VALUES(2800,1,2,1000,837,250);-- Malboro Fiber / NMs Zipacna
+INSERT INTO mob_droplist VALUES(2800,1,3,1000,28608,500);-- Earthcry Mantle / NMs Zipacna
+INSERT INTO mob_droplist VALUES(2800,1,3,1000,21252,500);-- One-eyed / NMs Zipacna
+INSERT INTO mob_droplist VALUES(1397,1,1,1000,4067,250);-- Rem tale 4 / NMs Jailer of Fortitude
+INSERT INTO mob_droplist VALUES(1397,1,1,1000,4068,250);-- Rem tale 5 / NMs Jailer of Fortitude
+INSERT INTO mob_droplist VALUES(1397,1,2,1000,836,250);-- Damascene Cloth / NMs Jailer of Fortitude
+INSERT INTO mob_droplist VALUES(1397,1,2,1000,1110,250);-- Beetle Blood / NMs Jailer of Fortitude
+INSERT INTO mob_droplist VALUES(1397,1,3,1000,20767,333);-- Medicor Sword / NMs Jailer of Fortitude
+INSERT INTO mob_droplist VALUES(1397,1,3,1000,20818,333);-- Hurlbat / NMs Jailer of Fortitude
+INSERT INTO mob_droplist VALUES(1397,1,3,1000,28455,334);-- Ovate Rope / NMs Jailer of Fortitude
+INSERT INTO mob_droplist VALUES(1402,1,1,1000,4068,250);-- Rem tale 5 / NMs Jailer of Temperance
+INSERT INTO mob_droplist VALUES(1402,1,1,1000,4064,250);-- Rem tale 1 / NMs Jailer of Temperance
+INSERT INTO mob_droplist VALUES(1402,1,2,1000,1311,250);-- Oxblood / NMs Jailer of Temperance
+INSERT INTO mob_droplist VALUES(1402,1,2,1000,836,250);-- Damascene Cloth / NMs Jailer of Temperance
+INSERT INTO mob_droplist VALUES(1402,1,3,1000,21045,500);-- Bukyoku / NMs Jailer of Temperance
+INSERT INTO mob_droplist VALUES(1402,1,3,1000,28609,500);-- Savior Mantle / NMs Jailer of Temperance
+INSERT INTO mob_droplist VALUES(1396,1,1,1000,4069,250);-- Rem tale 6 / NMs Jailer of Faith
+INSERT INTO mob_droplist VALUES(1396,1,1,1000,4070,250);-- Rem tale 7 / NMs Jailer of Faith
+INSERT INTO mob_droplist VALUES(1396,1,2,1000,844,250);-- Phoenix Feather / NMs Jailer of Faith
+INSERT INTO mob_droplist VALUES(1396,1,2,1000,1311,250);-- Oxblood / NMs Jailer of Faith
+INSERT INTO mob_droplist VALUES(1396,1,3,1000,28572,500);-- jwalamukhi Ring / NMs Jailer of Faith
+INSERT INTO mob_droplist VALUES(1396,1,3,1000,27735,500);-- Enedron Glasses / NMs Jailer of Faith
+INSERT INTO mob_droplist VALUES(1399,1,1,1000,4070,250);-- Rem tale 7 / NMs Jailer of Justice
+INSERT INTO mob_droplist VALUES(1399,1,1,1000,4071,250);-- Rem tale 8 / NMs Jailer of Justice
+INSERT INTO mob_droplist VALUES(1399,1,2,1000,837,250);-- Malboro Fiber / NMs Jailer of Justice
+INSERT INTO mob_droplist VALUES(1399,1,2,1000,844,250);-- Phoenix Feather / NMs Jailer of Justice
+INSERT INTO mob_droplist VALUES(1399,1,3,1000,21000,333);-- Magorokuhocho / NMs Jailer of Justice
+INSERT INTO mob_droplist VALUES(1399,1,3,1000,17528,333);-- Astral Signa / NMs Jailer of Justice
+INSERT INTO mob_droplist VALUES(1399,1,3,1000,20866,334);-- Parashu / NMs Jailer of Justice
+INSERT INTO mob_droplist VALUES(1398,1,1,1000,4071,250);-- Rem tale 8 / NMs Jailer of Hope
+INSERT INTO mob_droplist VALUES(1398,1,1,1000,4072,250);-- Rem tale 9 / NMs Jailer of Hope
+INSERT INTO mob_droplist VALUES(1398,1,2,1000,1110,250);-- Beetle Blood / NMs Jailer of Hope
+INSERT INTO mob_droplist VALUES(1398,1,2,1000,837,250);-- Malboro Fiber / NMs Jailer of Hope
+INSERT INTO mob_droplist VALUES(1398,1,3,1000,20541,500);-- Pinion Cesti / NMs Jailer of Hope
+INSERT INTO mob_droplist VALUES(1398,1,3,1000,20999,500);-- Habukatana / NMs Jailer of Hope
+INSERT INTO mob_droplist VALUES(1401,1,1,1000,4072,250);-- Rem tale 9 / NMs Jailer of Prudance
+INSERT INTO mob_droplist VALUES(1401,1,1,1000,4073,250);-- Rem tale 10 / NMs Jailer of Prudance
+INSERT INTO mob_droplist VALUES(1401,1,2,1000,836,250);-- Damascene Cloth / NMs Jailer of Prudance
+INSERT INTO mob_droplist VALUES(1401,1,2,1000,1110,250);-- Beetle Blood / NMs Jailer of Prudance
+INSERT INTO mob_droplist VALUES(1401,1,3,1000,21185,500);-- Boonwell Staff / NMs Jailer of Prudance
+INSERT INTO mob_droplist VALUES(1401,1,3,1000,20729,500);-- Vivifiante / NMs Jailer of Prudance
+INSERT INTO mob_droplist VALUES(1400,1,1,1000,4073,250);-- Rem tale 10 / NMs Jailer of love
+INSERT INTO mob_droplist VALUES(1400,1,1,1000,4069,250);-- Rem tale 6 / NMs Jailer of love
+INSERT INTO mob_droplist VALUES(1400,1,2,1000,1311,250);-- Oxblood / NMs Jailer of love
+INSERT INTO mob_droplist VALUES(1400,1,2,1000,836,250);-- Damascene Cloth / NMs Jailer of love
+INSERT INTO mob_droplist VALUES(1400,1,3,1000,20627,500);-- Surcoufs Jambiya / NMs Jailer of love
+INSERT INTO mob_droplist VALUES(1400,1,3,1000,20727,500);-- Tabahi Fleuret / NMs Jailer of love
+INSERT INTO mob_droplist VALUES(3,1,1,1000,4069,750);-- Rem tale 6 / NMs Absolue Virtue
+INSERT INTO mob_droplist VALUES(3,1,1,1000,4070,750);-- Rem tale 7 / NMs Absolue Virtue
+INSERT INTO mob_droplist VALUES(3,1,2,1000,4071,750);-- Rem tale 8 / NMs Absolue Virtue
+INSERT INTO mob_droplist VALUES(3,1,2,1000,4072,750);-- Rem tale 9 / NMs Absolue Virtue
+INSERT INTO mob_droplist VALUES(3,1,3,1000,4073,750);-- Rem tale 10 / NMs Absolue Virtue
+INSERT INTO mob_droplist VALUES(2196,1,3,1000,4070,250);-- Rem tale 7 / NMs Seiryu
+INSERT INTO mob_droplist VALUES(2196,1,3,1000,4071,250);-- Rem tale 8 / NMs Seiryu
+INSERT INTO mob_droplist VALUES(2196,1,4,1000,836,250);-- Damascene Cloth / NMs Seiryu
+INSERT INTO mob_droplist VALUES(2196,1,4,1000,1110,250);-- Beetle Blood / NMs Seiryu
+INSERT INTO mob_droplist VALUES(2196,1,5,1000,21232,333);-- Phulax Bow / NMs Seiryu
+INSERT INTO mob_droplist VALUES(2196,1,5,1000,20911,333);-- Last Rest / NMs Seiryu
+INSERT INTO mob_droplist VALUES(2196,1,5,1000,17928,334);-- Juggernaut / NMs Seiryu
+INSERT INTO mob_droplist VALUES(2362,1,3,1000,4071,250);-- Rem tale 8 / NMs Suzaku
+INSERT INTO mob_droplist VALUES(2362,1,3,1000,4072,250);-- Rem tale 9 / NMs Suzaku
+INSERT INTO mob_droplist VALUES(2362,1,4,1000,1311,250);-- Oxblood / NMs Suzaku
+INSERT INTO mob_droplist VALUES(2362,1,4,1000,836,250);-- Damascene Cloth / NMs Suzaku
+INSERT INTO mob_droplist VALUES(2362,1,5,1000,28026,500);-- Aiwon Gauntlets / NMs Suzaku
+INSERT INTO mob_droplist VALUES(2362,1,5,1000,28573,500);-- Wuji Ring / NMs Suzaku
+INSERT INTO mob_droplist VALUES(394,1,3,1000,4072,250);-- Rem tale 9 / NMs Byakko
+INSERT INTO mob_droplist VALUES(394,1,3,1000,4073,250);-- Rem tale 10 / NMs Byakko
+INSERT INTO mob_droplist VALUES(394,1,4,1000,844,250);-- Phoenix Feather / NMs Byakko
+INSERT INTO mob_droplist VALUES(394,1,4,1000,1311,250);-- Oxblood / NMs Byakko
+INSERT INTO mob_droplist VALUES(394,1,5,1000,21046,500);-- Kiikanemitsu / NMs Byakko
+INSERT INTO mob_droplist VALUES(394,1,5,1000,21184,500);-- Surma Staff / NMs Byakko
+INSERT INTO mob_droplist VALUES(946,1,3,1000,4073,250);-- Rem tale 10 / NMs Genbu
+INSERT INTO mob_droplist VALUES(946,1,3,1000,4070,250);-- Rem tale 7 / NMs Genbu
+INSERT INTO mob_droplist VALUES(946,1,4,1000,1110,250);-- Beetle Blood / NMs Genbu
+INSERT INTO mob_droplist VALUES(946,1,4,1000,836,250);-- Damascene Cloth / NMs Genbu
+INSERT INTO mob_droplist VALUES(946,1,5,1000,21423,500);-- Tardus Grip / NMs Genbu
+INSERT INTO mob_droplist VALUES(946,1,5,1000,21282,500);-- Qasama Hexagun / NMs Genbu
+INSERT INTO mob_droplist VALUES(2843,1,2,1000,4064,250);-- Rem tale 1 / NMs Ix'aern (DRG)
+INSERT INTO mob_droplist VALUES(2843,1,2,1000,4065,250);-- Rem tale 2 / NMs Ix'aern (DRG)
+INSERT INTO mob_droplist VALUES(2843,1,3,1000,844,250);-- Phoenix Feather / NMs Ix'aern (DRG)
+INSERT INTO mob_droplist VALUES(2843,1,3,1000,1311,250);-- Oxblood / NMs Ix'aern (DRG)
+INSERT INTO mob_droplist VALUES(2843,1,4,1000,28658,333);-- Sors Shield / NMs Ix'aern (DRG)
+INSERT INTO mob_droplist VALUES(2843,1,4,1000,20955,333);-- Agilis Lance / NMs Ix'aern (DRG)
+INSERT INTO mob_droplist VALUES(2843,1,4,1000,21424,334);-- Zuuxowu Grip / NMs Ix'aern (DRG)
+INSERT INTO mob_droplist VALUES(2844,1,2,1000,4066,250);-- Rem tale 3 / NMs Ix'aern (DRK)
+INSERT INTO mob_droplist VALUES(2844,1,2,1000,4067,250);-- Rem tale 4 / NMs Ix'aern (DRK)
+INSERT INTO mob_droplist VALUES(2844,1,3,1000,1110,250);-- Beetle Blood / NMs Ix'aern (DRK)
+INSERT INTO mob_droplist VALUES(2844,1,3,1000,837,250);-- Malboro Fiber / NMs Ix'aern (DRK)
+INSERT INTO mob_droplist VALUES(2844,1,4,1000,20957,500);-- Concido Course / NMs Ix'aern (DRK)
+INSERT INTO mob_droplist VALUES(2844,1,4,1000,20626,500);-- Blitto Needle / NMs Ix'aern (DRK)
+INSERT INTO mob_droplist VALUES(2819,1,3,1000,4069,750);-- Rem tale 6 / NMs Kirin
+INSERT INTO mob_droplist VALUES(2819,1,3,1000,4070,750);-- Rem tale 7 / NMs Kirin
+INSERT INTO mob_droplist VALUES(2819,1,4,1000,4071,750);-- Rem tale 8 / NMs Kirin
+INSERT INTO mob_droplist VALUES(2819,1,4,1000,4072,750);-- Rem tale 9 / NMs Kirin
+INSERT INTO mob_droplist VALUES(2819,1,5,1000,4073,750);-- Rem tale 10 / NMs Kirin
+INSERT INTO mob_droplist VALUES(3919,1,1,1000,4065,250);-- Rem tale 2 / NMs Ix'aern (MNK)
+INSERT INTO mob_droplist VALUES(3919,1,1,1000,4066,250);-- Rem tale 3 / NMs Ix'aern (MNK)
+INSERT INTO mob_droplist VALUES(3919,1,2,1000,837,250);-- Malboro Fiber / NMs Ix'aern (MNK)
+INSERT INTO mob_droplist VALUES(3919,1,2,1000,844,250);-- Phoenix Feather / NMs Ix'aern (MNK)
+INSERT INTO mob_droplist VALUES(3919,1,3,1000,20819,500);-- Antican Axe / NMs Ix'aern (MNK)
+INSERT INTO mob_droplist VALUES(3919,1,3,1000,20730,500);-- Predatrice / NMs Ix'aern (MNK)
