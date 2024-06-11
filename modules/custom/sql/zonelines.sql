cc1bc611248f55cbd5c62207ed063bc78c71fbe8
General <75862857+FFXILevelDown@users.noreply.github.com>
2024-06-10 19:53:35 -0600

    addin zone lines for Woh to Cidras Caverns, lessen change for IxAern DRK to respawn

---
+++ modules/custom/sql/zonelines.sql
@@ -0,0 +1,5 @@
+INSERT INTO zonelines VALUES (913061754, 273, 270, -218.0415,  9.5936, 522.4333,  60); -- WoH to Cirdas Caverns
+INSERT INTO zonelines VALUES (812201850, 270, 273,  402.9207, 30.6611,  98.1177, 139); -- Cirdas Caverns to Woh
+
+
+
