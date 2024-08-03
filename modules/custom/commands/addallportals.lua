3e7f10b2252f9b4b61a68bf941e0cfc704add922
ShadowwindFFXI <163480182+ShadowwindFFXI@users.noreply.github.com>
2024-08-02 18:20:08 -0600

    Updates to add all ports

--- modules/custom/commands/addallportals.lua
+++ modules/custom/commands/addallportals.lua
@@ -109,62 +109,6 @@ commandObj.onTrigger = function(player, target)
         {8, 7},
     }
 
-    local adoulinPortals =
-    {
-        {0, 0},
-        {1, 1},
-        {2, 2},
-        {3, 3},
-        {4, 4},
-        {5, 5},
-        {6, 6},
-        {7, 7},
-        {8, 8},
-        {15, 9},
-        {16, 10},
-        {17, 11},
-        {18, 12},
-        {19, 13},
-        {20, 14},
-        {21, 15},
-        {22, 16},
-        {23, 17},
-        {38, 22},
-        {39, 23},
-        {40, 24},
-        {41, 25},
-        {32, 18},
-        {33, 19},
-        {34, 20},
-        {35, 21},
-        {64, 26},
-        {65, 27},
-        {66, 28},
-        {67, 29},
-        {68, 30},
-        {70, 31},
-        {71, 32},
-        {72, 33},
-        {73, 34},
-        {74, 35},
-        {75, 36},
-        {96, 37},
-        {97, 38},
-        {98, 39},
-        {99, 40},
-        {102, 41},
-        {103, 42},
-        {104, 43},
-        {105, 44},
-        {106, 45},
-        {134, 46},
-        {135, 47},
-        {136, 48},
-        {137, 49},
-        {138, 50},
-    }
-
-
     -- validate target
     local targ = player
 
@@ -179,10 +123,11 @@ commandObj.onTrigger = function(player, target)
         targ:addTeleport(xi.teleport.type.ESCHAN_PORTAL, v)
     end
     -- add all waypoints
-    for _, v in ipairs(adoulinPortals) do
-        targ:addTeleport(xi.teleport.type.WAYPOINT, v[1], v[2])
+    for i = 1,50 do
+    targ:addTeleport(xi.teleport.type.WAYPOINT, i)
     end
-    player:printToPlayer(string.format('%s now has all Abyssea Confluxs.', targ:getName()))
+
+    player:printToPlayer(string.format('%s now has all Abyssea Confluxs, Escha Portals and Adoulin Waypoints.', targ:getName()))
 end
 
 return commandObj
