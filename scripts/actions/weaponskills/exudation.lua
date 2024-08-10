a6af615b780af8878b71cc4d45f1333ee21c4e96
General <75862857+FFXILevelDown@users.noreply.github.com>
2024-08-09 18:02:22 -0600

    Added aftermath effect to Exudation WS

--- scripts/actions/weaponskills/exudation.lua
+++ scripts/actions/weaponskills/exudation.lua
@@ -20,6 +20,9 @@ weaponskillObject.onUseWeaponSkill = function(player, target, wsID, tp, primary,
         params.int_wsc = 0.5 params.mnd_wsc = 0.5
     end
 
+        -- Apply Aftermath
+    xi.aftermath.addStatusEffect(player, tp, xi.slot.MAIN, xi.aftermath.type.MYTHIC)
+
     local damage, criticalHit, tpHits, extraHits = xi.weaponskills.doPhysicalWeaponskill(player, target, wsID, params, tp, action, primary, taChar)
     return tpHits, extraHits, criticalHit, damage
 end
